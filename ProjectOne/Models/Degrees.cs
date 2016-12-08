7a5a01b803edac9afabb589456752abefe745997
Josh-Bean <joshua.bean55@gmail.com>
2016-12-07 17:00:27 -0700

    Connection String, Degree Model
    Good stuff baby

---
+++ ProjectOne/Models/Degrees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectOne.Models
+{
+    public class Degrees
+    {
+        [Key]
+        public int DegreeID { get; set; }
+        public String Name { get; set; }
+        public String Coordinator { get; set; }
+        public String CoordinatorTitle { get; set;}
+        public String CoordinatorOffice { get; set; }
+        public String PhDEducation { get; set; }
+        public String BachelorsEducation { get; set; }
+        public String CoordinatorPicture { get; set; }
+    }
+}
