c6428563ac0d544ad58cda1abcf4f446ef94a99c
Josh-Bean <joshua.bean55@gmail.com>
2016-12-07 18:54:04 -0700

    DAL
    Creation of DAL

---
+++ ProjectOne/DAL/DegreeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectOne.Models;
+using System.Data.Entity;
+
+namespace ProjectOne.DAL
+{
+    public class DegreeContext : DbContext
+    {
+        public DegreeContext() :base("DegreeContext")
+        {
+
+        }
+
+        public DbSet<Users> User { get; set; }
+        public DbSet<Degrees> Degree { get; set; }
+        public DbSet<DegreeQuestions> DegreeQuestion { get; set; }
+    }
+}
