5dbc533ab584e729b9afc0f802829c08bc2a3eb2
Josh-Bean <joshua.bean55@gmail.com>
2016-12-07 17:10:19 -0700

    Degree Questions model
    Yay

---
+++ ProjectOne/Models/DegreeQuestions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectOne.Models
+{
+    public class DegreeQuestions
+    {
+        [Key]
+        public int DegreeQuestionID { get; set; }
+        public int DegreeID { get; set; }
+        public int UserID { get; set; }
+        public String Question { get; set; }
+        public String Answer { get; set; }
+    }
+}
