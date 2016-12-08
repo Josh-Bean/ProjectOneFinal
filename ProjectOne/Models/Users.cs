ffe1a14b39b8cd81cd79ff5589c9063707d637a2
Josh-Bean <joshua.bean55@gmail.com>
2016-12-07 17:07:02 -0700

    Users Model
    Made a users model

---
+++ ProjectOne/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectOne.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
